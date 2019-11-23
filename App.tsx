09b2e2fe78f1303677fc56b3b2b15be20aff25e1
Dharmen Shah <dharmen@quarkstudios.com>
2019-11-23 23:24:37 +0530

    added sentry

--- App.tsx
+++ App.tsx
@@ -5,6 +5,17 @@ import { Ionicons } from "@expo/vector-icons";
 import Main from "./src/Main";
 import { StyleProvider, Root } from "native-base";
 import getTheme from "./native-base-theme/components";
+import Constants from "expo-constants";
+
+import * as Sentry from "sentry-expo";
+
+Sentry.init({
+  dsn: "https://9490cecf0fc94305aef86e2f53b0ef26@sentry.io/1832015",
+  enableInExpoDevelopment: true,
+  debug: true
+});
+
+Sentry.setRelease(Constants.manifest.revisionId);
 
 type Props = {};
 
